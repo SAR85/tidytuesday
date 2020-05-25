bdc8699ab89dcc0d33446175f936f7f68856abcf
Sean <flsar85@gmail.com>
2020-05-25 13:06:22 -0700

    Add 052620 directory and script

---
+++ 052620/052620_cocktails.R
@@ -0,0 +1,5 @@
+library(tidyverse)
+
+cocktails <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-05-26/cocktails.csv')
+boston_cocktails <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-05-26/boston_cocktails.csv')
+
